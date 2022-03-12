3cdd9b625918216cb9524495d33af470c348b5c4
madinaalizada@outlook.com <madinaalizada@outlook.com>
2022-03-12 19:30:23 +0200

    Add project files.

---
+++ Maliza/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Maliza
+{
+    class Program
+    {
+        interface IKitab
+        {
+            string Autor { get; set; }
+            string Type { get; set; }
+            int Coust { get; set; }
+            bool OnlineOrder { get; set; }
+            int DeliveryCoust { get; set; }
+            bool OxunmaSeviyyesi(int x);
+        }
+        class Kitab : IKitab
+        {
+            string IKitab.Autor { get ; set ; }
+            string IKitab.Type { get ; set ; }
+            int IKitab.Coust { get; set ; }
+            bool IKitab.OnlineOrder { get ; set ; }
+            int IKitab.DeliveryCoust { get; set ; }
+
+            bool IKitab.OxunmaSeviyyesi(int x)
+            {
+                if(x>100)
+                    return true;
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            IKitab n = new Kitab();
+            n.Autor = "Stefan Zweig";
+            n.Type = "Hekaye";
+            n.Coust = 4;
+            n.DeliveryCoust = 2;
+            n.OnlineOrder = true;
+        }
+    }
+}
---
+++ interface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interface
+{
+    interface IMedine
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+    }
+    class Person : IMedine
+    {
+        public string Name { get ; set ; }
+        public string Surname { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
